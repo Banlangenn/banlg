63bf426a3d16bc154fe96e4c55399afa3bbd57a3
banlangen <humingx@yeah.net928012047@qq.com>
2018-12-18 14:15:18 +0800

    [build] 1.0.18

--- release.sh
+++ release.sh
@@ -18,15 +18,9 @@ then
   # commit
   git add -A
   git commit -m "[build] $VERSION"
-  npm version $VERSION --message "[release] $VERSION"
 
-  # publish
-  git push eleme master
-  git push eleme refs/tags/v$VERSION
-  git checkout dev
-  git rebase master
-  git push eleme dev
 
+  
   if [[ $VERSION =~ "beta" ]]
   then
     npm publish --tag beta
